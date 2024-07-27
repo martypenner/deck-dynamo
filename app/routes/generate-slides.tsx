ba022d3059870961407d6c0a57ee5c503c1729fc
Marty Penner <martypenner@users.noreply.github.com>
2024-07-26 20:08:57 -0400

    feat: allow browsing previous generations in the logs

--- app/routes/generate-slides.tsx
+++ app/routes/generate-slides.tsx
@@ -1,7 +1,14 @@
 import fs from 'node:fs'
 import path from 'node:path'
-import { Form, json, useActionData, useNavigation } from '@remix-run/react'
-import { openai, anthropic } from '#app/utils/ai-providers'
+import Anthropic from '@anthropic-ai/sdk'
+import {
+	json,
+	useActionData,
+	useFetcher,
+	useLoaderData,
+} from '@remix-run/react'
+import OpenAI from 'openai'
+import { type KeyboardEvent, useCallback, useEffect, useState } from 'react'
 
 type Slides = Array<
 	| {
@@ -20,7 +27,19 @@ type Slides = Array<
 	  }
 >
 
-const generateImage = async ({ prompt }: { prompt: string }) => {
+type Presentation = {
+	date: Date
+	topic: string
+	slides: string[]
+}
+
+const generateImage = async ({
+	openai,
+	prompt,
+}: {
+	openai: OpenAI
+	prompt: string
+}) => {
 	try {
 		const response = await openai.images
 			.generate({
@@ -43,13 +62,13 @@ const generateImage = async ({ prompt }: { prompt: string }) => {
 			const waitTime = parseInt(retryAfter, 10) * 1000 + jitter
 			console.info('waitTime, retryAfter', waitTime, retryAfter)
 			await new Promise((resolve) => setTimeout(resolve, waitTime))
-			return await generateImage({ prompt })
+			return await generateImage({ openai, prompt })
 		}
 		throw error
 	}
 }
 
-function logGeneratedContent({
+async function storeGeneratedContent({
 	topic,
 	slides,
 	imageUrls,
@@ -60,25 +79,43 @@ function logGeneratedContent({
 }) {
 	const datetime = new Date().toISOString().replace(/:/g, '-')
 	const kebabCaseTopic = topic.toLowerCase().replace(/\s+/g, '-')
-	const logDir = path.join('logs', `${datetime}-${kebabCaseTopic}`)
+	const logDir = path.join('public/logs', `${datetime}-${kebabCaseTopic}`)
 
 	fs.mkdirSync(logDir, { recursive: true })
 	fs.writeFileSync(path.join(logDir, 'topic.txt'), topic)
 
-	const slidesWithUrls = slides.map((slide, index) => ({
+	const imageDir = path.join(logDir, 'images')
+	fs.mkdirSync(imageDir, { recursive: true })
+
+	const fetchAndSaveImage = async (url: string, index: number) => {
+		const response = await fetch(url)
+		const arrayBuffer = await response.arrayBuffer()
+		const buffer = Buffer.from(arrayBuffer)
+		const imagePath = path.join(imageDir, `image_${index}.jpg`)
+		await fs.promises.writeFile(imagePath, buffer)
+		return imagePath
+	}
+
+	const imagePaths = await Promise.all(imageUrls.map(fetchAndSaveImage))
+
+	const slidesWithImagePaths = slides.map((slide, index) => ({
 		...slide,
 		...('image' in slide && {
 			image: {
 				...slide.image,
-				url: imageUrls[index],
+				path: imagePaths[index],
 			},
 		}),
 	}))
 
 	fs.writeFileSync(
 		path.join(logDir, 'slides.json'),
-		JSON.stringify(slidesWithUrls, null, 2),
+		JSON.stringify(slidesWithImagePaths, null, 2),
 	)
+}
+
+export async function loader() {
+	return json({ presentations: getPresentations() })
 }
 
 export async function action() {
@@ -90,28 +127,29 @@ export async function action() {
 		? `Ensure the first slide has the title along with a made-up name and a description of that person's job title or career accomplishments.`
 		: ``
 
-	const topicResponse = await anthropic.messages.create(
-		{
-			model: 'claude-3-5-sonnet-20240620',
-			max_tokens: 8000,
-			temperature: 0.8,
-			messages: [
-				{
-					role: 'user',
-					content: `
-					You are an expert improvisational slide deck creator.
-          Generate the outline for a random slide deck.
-					Tell me only the topic, not anything else.
-				`,
-				},
-			],
-		},
-		{
-			headers: {
-				'anthropic-beta': 'max-tokens-3-5-sonnet-2024-07-15',
+	const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY })
+	const anthropic = new Anthropic({ apiKey: process.env.ANTHROPIC_KEY })
+
+	const topicResponse = await anthropic.messages.create({
+		model: 'claude-3-5-sonnet-20240620',
+		max_tokens: 8000,
+		temperature: 0.8,
+		messages: [
+			{
+				role: 'user',
+				content: `
+						You are an expert improvisational slide deck creator.
+	          Generate the outline for a random slide deck.
+						Tell me only the topic, not anything else. Do not include a prelude, an explanation, or anything
+						other than the topic itself.
+
+						<example>
+						The Habits of Wealthy Chimpanzees
+						</example>
+					`,
 			},
-		},
-	)
+		],
+	})
 	const topic =
 		topicResponse.content[0]?.type === 'text'
 			? topicResponse.content[0]?.text
@@ -131,79 +169,79 @@ export async function action() {
 				{
 					role: 'user',
 					content: `
-					You are an expert improvisational slide deck creator.
-          Generate the outline for a random slide deck.
-					The slide deck should be pretty barebones to allow a presenter to improvise their way through.
-					This slide deck will be used in improv competitions, so it should not be continuous in topic
-					from slide to slide. Ensure that there are a few completely surprising left turns to keep things
-					dynamic. The slides should not tell the whole story in order to leave room for the improviser to
-					justify the slides contents.
+						You are an expert improvisational slide deck creator.
+	          Generate the outline for a random slide deck.
+						The slide deck should be pretty barebones to allow a presenter to improvise their way through.
+						This slide deck will be used in improv competitions, so it should not be continuous in topic
+						from slide to slide. Ensure that there are a few completely surprising left turns to keep things
+						dynamic. The slides should not tell the whole story in order to leave room for the improviser to
+						justify the slides contents.
 
-          The presentation topic is "${topic}".
+	          The presentation topic is "${topic}".
 
-          Focus more heavily on images instead of text on the slides. Any text you generate should be
-					overlaid onto images. Assume that the image URLs will be provided elsewhere, but describe them in
-					the JSON format I describe below. Only up to ${totalTextSlides} slides should contain text (you
-					will need to specify the text in the image prompts).
-					${openingSlide}
-					The last slide should contain the words "in conclusion" and a random image.
-					Generate ${totalSlides} slides, including the slides I've already described.
+	          Focus more heavily on images instead of text on the slides. Any text you generate should be
+						overlaid onto images. Assume that the image URLs will be provided elsewhere, but describe them in
+						the JSON format I describe below. Only up to ${totalTextSlides} slides should contain text (you
+						will need to specify the text in the image prompts).
+						${openingSlide}
+						The last slide should contain the words "in conclusion" and a random image.
+						Generate ${totalSlides} slides, including the slides I've already described.
 
-					Output in JSON format using the following schema. Do NOT provide any context, prelude, or
-					explanation; only give back the JSON.
-					\`\`\`
-{
-  "$schema": "https://json-schema.org/draft/2020-12/schema",
-  "type": "object",
-  "properties": {
-    "slides": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "oneOf": [
-          {
-            "properties": {
-              "title": {
-                "type": "object",
-                "properties": {
-                  "content": { "type": "string" }
-                },
-                "required": ["content", "alignment"]
-              },
-              "presenter": {
-                "type": "object",
-                "properties": {
-                  "name": { "type": "string" },
-                  "title": { "type": "string" }
-                },
-                "required": ["name", "description"]
-              }
-            },
-            "required": ["title", "presenter"],
-            "unevaluatedProperties": false
-          },
-          {
-            "properties": {
-              "image": {
-                "type": "object",
-                "properties": {
-                  "description": { "type": "string" }
-                },
-                "required": ["description"]
-              }
-            },
-            "required": ["image"],
-            "unevaluatedProperties": false
-          }
-        ]
-      },
-      "minItems": 1
-    }
-  },
-  "required": ["slides"]
-}
-					\`\`\`
-				`,
+						Output in JSON format using the following schema. Do NOT provide any context, prelude, or
+						explanation; only give back the JSON.
+						\`\`\`
+	{
+	  "$schema": "https://json-schema.org/draft/2020-12/schema",
+	  "type": "object",
+	  "properties": {
+	    "slides": {
+	      "type": "array",
+	      "items": {
+	        "type": "object",
+	        "oneOf": [
+	          {
+	            "properties": {
+	              "title": {
+	                "type": "object",
+	                "properties": {
+	                  "content": { "type": "string" }
+	                },
+	                "required": ["content", "alignment"]
+	              },
+	              "presenter": {
+	                "type": "object",
+	                "properties": {
+	                  "name": { "type": "string" },
+	                  "title": { "type": "string" }
+	                },
+	                "required": ["name", "description"]
+	              }
+	            },
+	            "required": ["title", "presenter"],
+	            "unevaluatedProperties": false
+	          },
+	          {
+	            "properties": {
+	              "image": {
+	                "type": "object",
+	                "properties": {
+	                  "description": { "type": "string" }
+	                },
+	                "required": ["description"]
+	              }
+	            },
+	            "required": ["image"],
+	            "unevaluatedProperties": false
+	          }
+	        ]
+	      },
+	      "minItems": 1
+	    }
+	  },
+	  "required": ["slides"]
+	}
+						\`\`\`
+					`,
 				},
 			],
 		},
@@ -241,6 +279,7 @@ export async function action() {
 
 					try {
 						const { url } = await generateImage({
+							openai,
 							prompt: slide.image.description,
 						})
 						console.info('Generated image:', url)
@@ -263,27 +302,133 @@ export async function action() {
 	await generateImages()
 	console.info(imageUrls)
 
-	logGeneratedContent({ topic, slides: slidesJson.slides, imageUrls })
+	// Probably shouldn't await this so that the server can respond more quickly, but meh
+	await storeGeneratedContent({ topic, slides: slidesJson.slides, imageUrls })
 
 	return json({ imageUrls })
 }
 
-export default function Testing() {
+function getPresentations(): Presentation[] {
+	const logDir = path.join(process.cwd(), 'public/logs')
+	const logs = fs.readdirSync(logDir)
+
+	const presentations = logs.map((generation) => {
+		const generationPath = path.join(logDir, generation)
+		const topicPath = path.join(generationPath, 'topic.txt')
+		const topic = fs.readFileSync(topicPath, 'utf-8').trim()
+
+		const [date] = generation.split('Z-')
+
+		const slidesPath = path.join(generationPath, 'slides.json')
+		const slides = JSON.parse(fs.readFileSync(slidesPath, 'utf-8')) as {
+			image: { path: string }
+		}[]
+
+		return {
+			date: new Date(date ?? ''),
+			topic,
+			slides: slides
+				.filter((slide) => 'image' in slide)
+				.map((slide) => '/logs/' + generation + '/' + slide.image.path),
+		}
+	})
+
+	presentations.sort((a, b) => {
+		if (a.date > b.date) return -1
+		if (a.date < b.date) return 1
+		return a.topic.localeCompare(b.topic)
+	})
+
+	return presentations
+}
+
+export default function GeneratePresentation() {
+	const { presentations } = useLoaderData<typeof loader>()
 	const data = useActionData<typeof action>()
 	const imageUrls = data?.imageUrls ?? []
-	const navigation = useNavigation()
+
+	const { Form, state } = useFetcher()
+	const [selectedTopic, setSelectedTopic] = useState(
+		imageUrls.length > 0 ? 'generated' : '',
+	)
+	console.log(selectedTopic)
+
+	const currentTopicSlides =
+		presentations
+			.find((presentation) => presentation.topic === selectedTopic)
+			?.slides?.slice(1) ?? imageUrls
+	console.dir(currentTopicSlides, { depth: null })
 
 	return (
-		<Form method="post">
-			<button type="submit" disabled={navigation.state !== 'idle'}>
-				{navigation.state === 'idle' ? 'Generate images' : 'Generating...'}
-			</button>
+		<>
+			<Form method="post">
+				<button type="submit" disabled={state !== 'idle'}>
+					{state === 'idle' ? 'Generate presentation' : 'Generating...'}
+				</button>
+			</Form>
 
-			<div>
-				{imageUrls.map((url) => (
-					<img key={url} src={url} alt="Generated slide image" />
+			<select
+				value={selectedTopic}
+				onChange={(e) => setSelectedTopic(e.target.value)}
+				className="text-black"
+			>
+				<option value="">Select a topic</option>
+				{imageUrls.length > 0 && (
+					<option value="generated">Generated presentation</option>
+				)}
+				{presentations.map((presentation) => (
+					<option key={presentation.topic} value={presentation.topic}>
+						{presentation.topic}
+					</option>
 				))}
+			</select>
+
+			{selectedTopic.trim() !== '' && (
+				<SlideNavigation slides={currentTopicSlides} />
+			)}
+		</>
+	)
+}
+
+const SlideNavigation = ({ slides }: { slides: string[] }) => {
+	const [currentSlide, setCurrentSlide] = useState(0)
+
+	const goToNextSlide = useCallback(() => {
+		setCurrentSlide((prev) => (prev + 1) % slides.length)
+	}, [slides.length])
+
+	const goToPreviousSlide = useCallback(() => {
+		setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length)
+	}, [slides.length])
+
+	useEffect(() => {
+		const handleKeyDown = (event: KeyboardEvent) => {
+			switch (event.key) {
+				case 'ArrowRight':
+				case ' ':
+					goToNextSlide()
+					break
+				case 'ArrowLeft':
+				case 'Backspace':
+					goToPreviousSlide()
+					break
+				default:
+					break
+			}
+		}
+
+		window.addEventListener('keydown', handleKeyDown)
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown)
+		}
+	}, [goToNextSlide, goToPreviousSlide])
+
+	return (
+		<div className="position-relative">
+			<img src={slides[currentSlide]} alt={`Slide ${currentSlide + 1}`} />
+			<div className="absolute bottom-2.5 right-2.5 rounded bg-black bg-opacity-50 px-2.5 py-1.5 text-xs text-white">
+				{currentSlide + 1} / {slides.length}
 			</div>
-		</Form>
+		</div>
 	)
 }
--- app/utils/ai-providers.ts
+++
@@ -1,5 +0,0 @@
-import Anthropic from '@anthropic-ai/sdk'
-import OpenAI from 'openai'
-
-export const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY })
-export const anthropic = new Anthropic({ apiKey: process.env.ANTHROPIC_KEY })
