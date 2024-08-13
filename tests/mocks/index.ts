c400e3635754b65e730275e4af6abb3572be79d9
Marty Penner <martypenner@users.noreply.github.com>
2024-08-13 19:24:32 -0400

    chore: move an import

--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -1,9 +1,9 @@
 import closeWithGrace from 'close-with-grace'
 import { setupServer } from 'msw/node'
+import { handlers as anthropicHandlers } from './anthropic'
 import { handlers as githubHandlers } from './github'
 import { handlers as replicateHandlers } from './replicate'
 import { handlers as resendHandlers } from './resend'
-import { handlers as anthropicHandlers } from './anthropic'
 
 export const server = setupServer(
 	...resendHandlers,
